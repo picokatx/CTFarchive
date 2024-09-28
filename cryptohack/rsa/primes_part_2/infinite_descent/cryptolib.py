d64fb2d4e27e9cd20895e746a2bd2df0b5c85527
picokatx <picokatx@gmail.com>
2024-09-28 21:39:17 +0800

    feat: couple rsas

--- cryptohack/rsa/primes_part_2/infinite_descent/cryptolib.py
+++ cryptohack/rsa/primes_part_2/infinite_descent/cryptolib.py
@@ -152,27 +152,6 @@ def pollard(n):
         if (d > 1):
             return d
         i += 1
-def isqrt(n):
-	x=n
-	y=(x+n//x)//2
-	while(y<x):
-		x=y
-		y=(x+n//x)//2
-	return x
-def fermat(n):
-	t0=isqrt(n)+1
-	counter=0
-	t=t0+counter
-	temp=isqrt((t*t)-n)
-	while((temp*temp)!=((t*t)-n)):
-		counter+=1
-		t=t0+counter
-		temp=isqrt((t*t)-n)
-	s=temp
-	p=t+s
-	q=t-s
-	return p,q
-
 from sympy.solvers import solve
 from sympy import Symbol
 from sympy.ntheory.continued_fraction import continued_fraction_convergents
